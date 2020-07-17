87885f2288014695a11c98d7176a79c3e155ebcf
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-17 03:07:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster ={}
+
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+
+    @roster[@grade] = []
+    @roster[@grade].push(@name)
+
+
+  end
+
+  def grade
+
+  end
+
+  def sort
+
+  end
+
+
+
+
+
+
+end
