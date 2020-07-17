ae38e39be3b1d6fe9d58f72f28e1ca57fc3d4275
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-17 04:06:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,64 @@
 # code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster ={}
+
+  end
+
+  def roster
+    @roster
+  end
+
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+
+    if @roster.key?(@grade)
+      @roster[@grade] << @name
+    else
+      @roster[@grade] = [name]
+
+
+    end
+  end
+
+
+
+
+
+  def grade(grade)
+    @roster[grade]
+
+
+
+  end
+
+  def sort
+    sorted_list = {}
+    @roster.each do |k, v|
+      sorted_list[k] = v.sort
+      
+
+
+    end
+    sorted_list
+
+
+
+
+
+
+
+  end
+
+
+
+
+
+
+end
